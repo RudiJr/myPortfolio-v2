11d2fcdf9074afb19e90c73ca765fbd7b45909b3
RudiJr <rudi.juniior@gmail.com>
2023-10-03 11:59:45 -0300

    Creating the contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,22 @@
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <section>
+      <h1 className="title">Contact <span>Me</span></h1>
+      <div className="media-links">
+        <div className="social-box">
+          <div className="social-card">
+            <a href="https://www.linkedin.com/in/rudi-junior/"><i className="bx bxl-linkedin"></i></a>
+            <h5>LinkedIn</h5>
+          </div>
+
+          <div className="social-card">
+            <a href="https://github.com/RudiJr"><i className="bx bxl-github"></i></a>
+            <h5>Github</h5>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
