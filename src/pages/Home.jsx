5d599e1452bb44f112e481bd8e2776e3694ee9ae
RudiJr <rudi.juniior@gmail.com>
2023-10-03 11:49:42 -0300

    Creating the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
+import Header from "@/components/Header";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <Header />,
+    <section>
+        <div className="profile-content">
+        <h3>Hello, my name is</h3>
+        <h1>Rudi Júnior</h1>
+        <h3>{`And i'm a`}<span className="multiple-text"></span></h3>
+
+        <div className="social-media">
+          <Link href="https://linkedin.com/in/rudi-junior" target="_blank"><i className="bx bxl-linkedin" title="My LinkedIn"></i></Link>
+          <Link href="https://github.com/RudiJr" target="_blank"><i className="bx bxl-github" title="My Github"></i></Link>
+        </div>
+      </div>
+
+      <div className="profile-img">
+        <img src="" alt="Rudi Júnior picture" />
+      </div>
+    </section>
   );
 };
 
