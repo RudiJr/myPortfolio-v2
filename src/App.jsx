37caf7768de76488c9b54a0a5a58a69dfced3aae
RudiJr <rudi.juniior@gmail.com>
2023-10-03 00:25:46 -0300

    Importing header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './index.css'
+import './index.css';
+import Header from '@/components/Header';
+
 
 function App() {
-
   return (
     <>
-     
+        <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
