f5eb04a7832baa5feec60a2473fcdf75aa41fe68
RudiJr <rudi.juniior@gmail.com>
2023-10-03 12:13:57 -0300

    Updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './index.css';
 import Header from '@/components/Header';
-import Home from '@/pages'
 
 
 function App() {
   return (
     <>
-        <Header />,
-        <Home />
+        <Header />
     </>
   );
 }
