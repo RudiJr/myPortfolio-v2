1802a5ca96d66bbc164f59c15fa167e1ac503c20
RudiJr <rudi.juniior@gmail.com>
2023-10-03 12:07:18 -0300

    Creating Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+
+export const Footer = () => {
+  return (
+        <footer className="footer">
+            <h4>&copy; 2023 Rudi Júnior </h4>
+            <p>rudi.juniior@gmail.com</p> 
+        </footer>
+  );
+};
