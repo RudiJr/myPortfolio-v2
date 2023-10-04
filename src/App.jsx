c1795b0f1b363e30d3ea497c61adc147fbd23332
RudiJr <rudi.juniior@gmail.com>
2023-10-03 23:42:07 -0300

    Bring the nav routes into the app to fix problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './index.css';
-import Header from '@/components/Header';
+import {Route,Routes} from "react-router-dom"
+import {Home, About, Projects, Contact} from '@/pages'
 import Footer from '@/components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
         <Header />
+        <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
         <Footer />
     </>
   );
