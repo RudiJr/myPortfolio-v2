2283707833073e346182752afa34850b13b24ae6
RudiJr <rudi.juniior@gmail.com>
2023-10-03 23:38:45 -0300

    Bring nav into header manually to fix problems

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
-import {Route,Routes} from "react-router-dom"
-import Navbar from './Navbar';
-
-import {Home, About, Projects, Contact} from '@/pages'
+import {Link, NavLink} from "react-router-dom";
 
 const Header = () => {
   return (
-        <header className="header">
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-        </header>
+    <header className="header">
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className="nav-items">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-items">
+            <NavLink to="/about">About Me</NavLink>
+          </li>
+          <li className="nav-items">
+            <NavLink to="/projects">Projects</NavLink>
+          </li>
+          <li className="nav-items">
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
