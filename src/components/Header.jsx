f080549f9a41c304e362b682f9bacc07c4ea5761
RudiJr <rudi.juniior@gmail.com>
2023-10-08 01:26:55 -0300

    Created onclick event to toggle and hide the menu in mobile version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,40 @@
 import {Link, NavLink} from "react-router-dom";
 import { useRef } from 'react';
+import { useState } from 'react';
 
 function Nav() {
   const iconRef = useRef();
+  const [clicked, setClicked] = useState();
+
   const handleClick = () => {
-    console.log('you clicked')
+    setClicked(!clicked);
+    if(clicked) {
+      iconRef.current.setAttribute("name", "menu");
+    }
+    if(!clicked) {
+      iconRef.current.setAttribute("name", "x");
+    }
+  };
+  
+  const divStyle = {
+    display: clicked ? 'flex' : 'none'
   };
 
   return (
     <header className="header">
       <box-icon onClick={handleClick} ref={iconRef} name='menu' color='#8d50d7' size='4rem'></box-icon>
-      <nav className="navbar">
+      <nav style={divStyle} className="navbar">
         <ul className="nav-list">
-          <li className="nav-items">
+          <li onClick={handleClick} className="nav-items">
             <Link to="/">Home</Link>
           </li>
-          <li className="nav-items">
+          <li onClick={handleClick} className="nav-items">
             <NavLink to="/about">About Me</NavLink>
           </li>
-          <li className="nav-items">
+          <li onClick={handleClick} className="nav-items">
             <NavLink to="/projects">Projects</NavLink>
           </li>
-          <li className="nav-items">
+          <li onClick={handleClick} className="nav-items">
             <NavLink to="/contact">Contact</NavLink>
           </li>
         </ul>
