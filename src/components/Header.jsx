d76962a801f12cfc9324c7b1caa78ed739bdfd8d
RudiJr <rudi.juniior@gmail.com>
2023-10-09 18:12:36 -0300

    Fixed navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,39 @@
 import {Link, NavLink} from "react-router-dom";
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect  } from 'react';
 
 function Nav() {
   const iconRef = useRef();
   const [clicked, setClicked] = useState();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Este useEffect só será executado uma vez após a montagem do componente
 
   const handleClick = () => {
-    setClicked(!clicked);
-    if(clicked) {
-      iconRef.current.setAttribute("name", "menu");
+    if (windowWidth < 640) {
+      setClicked(!clicked);
+      if(clicked) {
+        iconRef.current.setAttribute("name", "menu");
+      }
+      if(!clicked) {
+        iconRef.current.setAttribute("name", "x");
+      }
     }
-    if(!clicked) {
-      iconRef.current.setAttribute("name", "x");
-    }
-  };
-  
-  const divStyle = {
-    display: clicked ? 'flex' : 'none'
   };
 
   return (
     <header className="header">
       <box-icon onClick={handleClick} ref={iconRef} name='menu' color='#8d50d7' size='4rem'></box-icon>
-      <nav style={divStyle} className="navbar">
+      <nav style={{ display: windowWidth < 640 ? (clicked ? 'flex' : 'none') : 'flex' }} className="navbar">
         <ul className="nav-list">
           <li onClick={handleClick} className="nav-items">
             <Link to="/">Home</Link>
