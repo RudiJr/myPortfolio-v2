7347f7820c17e08bbb23dba54308f8c2e98b477d
RudiJr <rudi.juniior@gmail.com>
2023-10-18 15:20:59 -0300

    Fixed home section links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
-// import { Link } from "react-router-dom";
-
-export const Home = () => {
+export const Home = () => { 
   return (
     <section className="profile">
         <div className="profile-content">
@@ -9,8 +7,12 @@ export const Home = () => {
         <h3>{`And i'm a`}<span className="multiple-text"></span></h3>
 
         <div className="social-media">
-          {/* <Link href="https://linkedin.com/in/rudi-junior" target="_blank"><i className="bx bxl-linkedin" title="My LinkedIn"></i></Link>
-          <Link href="https://github.com/RudiJr" target="_blank"><i className="bx bxl-github" title="My Github"></i></Link> */}
+          <a href="https://linkedin.com/in/rudi-junior" rel="noreferrer" target="_blank">
+            <box-icon type='logo' title="My Linkedin" name='linkedin' color='#8544d5 ' size='2rem'></box-icon>
+          </a>
+          <a href="https://github.com/RudiJr" rel="noreferrer" target="_blank">
+            <box-icon name='github' title="My Github" type='logo' color='#8544d5' size='2rem'></box-icon>
+          </a>
         </div>
       </div>
 
