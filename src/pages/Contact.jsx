219fabe38f13db8a9b17f24dc95b06fb98846cd4
RudiJr <rudi.juniior@gmail.com>
2023-10-20 15:15:52 -0300

    changed the icon size

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,14 +6,14 @@ export const Contact = () => {
         <div className="social-box">
           <div className="social-card">
             <a href="https://www.linkedin.com/in/rudi-junior/" rel="noreferrer">
-              <box-icon type='logo' name='linkedin' color='#8544d5 ' size='3rem'></box-icon>
+              <box-icon type='logo' name='linkedin' color='#8544d5 ' size='2rem'></box-icon>
             </a>
             <h5>LinkedIn</h5>
           </div>
 
           <div className="social-card">
             <a href="https://github.com/RudiJr" rel="noreferrer">
-              <box-icon name='github' type='logo' color='#8544d5' size='3rem'></box-icon>
+              <box-icon name='github' type='logo' color='#8544d5' size='2rem'></box-icon>
             </a>
             <h5>Github</h5>
           </div>
@@ -23,5 +23,4 @@ export const Contact = () => {
   );
 };
 
-// TODO: Fix Links 
 
