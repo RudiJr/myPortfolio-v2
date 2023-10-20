ead7e7bce042fe54611287c189bd1cefb241e4bd
RudiJr <rudi.juniior@gmail.com>
2023-10-20 15:56:19 -0300

    Finalized about section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,22 @@
 export const Contact = () => {
   return (
-    <section>
-      <h1 className="title">Contact <span>Me</span></h1>
-      <div className="media-links">
+    <section className="contact">
+      <h2>Contact <span>Me</span></h2>
         <div className="social-box">
           <div className="social-card">
             <a href="https://www.linkedin.com/in/rudi-junior/" rel="noreferrer">
-              <box-icon type='logo' name='linkedin' color='#8544d5 ' size='2rem'></box-icon>
+              <box-icon type='logo' name='linkedin' color='#8544d5 ' size='3rem'></box-icon>
             </a>
             <h5>LinkedIn</h5>
           </div>
 
           <div className="social-card">
             <a href="https://github.com/RudiJr" rel="noreferrer">
-              <box-icon name='github' type='logo' color='#8544d5' size='2rem'></box-icon>
+              <box-icon name='github' type='logo' color='#8544d5' size='3rem'></box-icon>
             </a>
             <h5>Github</h5>
           </div>
         </div>
-      </div>
     </section>
   );
 };
